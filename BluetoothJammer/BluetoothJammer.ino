c2212c321df1350541d9b6e5b33965de29fdea47
Stuart Bermingham <stuartbermingham@gmail.com>
2025-05-25 00:32:29 +1200

    Added modes for BLE only and Bluetooth classic only which allows more jamming. Now using startFastWrite function for faster channel hopping. Serial output for profiling reduced to once each 5 seconds.

--- BluetoothJammer/BluetoothJammer.ino
+++ BluetoothJammer/BluetoothJammer.ino
@@ -22,75 +22,117 @@ RF24 radio1(22, 21, SPI_SPEED);
 // Second NRF24L01 on CE 4, CSN 2
 RF24 radio2(16, 15, SPI_SPEED);
 
-// Bluetooth channels - Each transmitter loops through half each
-int bluetooth_channels[] = {32, 34, 46, 48, 50, 52, 0, 1, 2, 4, 6, 8, 22, 24, 26, 28, 30, 74, 76, 78, 80};
+// BLE Advertising channels (2402MHz, 2426MHz, 2480MHz) - NRF24L01 uses different channel numbering
+int ble_channels[] = { 2, 26, 80 };
 
-const uint8_t startCh1 = 0;    // 2402 MHz
-const uint8_t endCh1   = 9;   // 2441 MHz
-const uint8_t startCh2 = 11;   // 2442 MHz
-const uint8_t endCh2   = 20;   // 2480 MHz
+// Bluetooth classic channels (2402–2480 MHz) - NRF24L01 channels are the same plus 2 MHz
+int bluetooth_channels[79];
+// Channels added in setup()
 
-// State
-boolean jammingOn = false;
-boolean buttonState = HIGH;
-boolean lastButtonState = HIGH;
+// Each radio transmits on one half of the channel range
+uint8_t num_channels = sizeof(ble_channels) / sizeof(ble_channels[0]);
+uint8_t split_index = num_channels / 2;
+uint8_t endCh1 = split_index - 1;
+uint8_t startCh2 = split_index;
+uint8_t endCh2 = num_channels - 1;
+
+// Start in BLE mode and press the button again to switch to classic channels
+bool classicMode = false;
+bool jammingOn = false;
+unsigned long lastHopTime = 0;
+const unsigned long hopInterval = 300;  // microseconds
+
+bool buttonState = HIGH;
+bool lastButtonState = HIGH;
 
 void setup() {
   // Give ESP32 extra time to start up
-  delay(1000);
+  delay(2000);
+
+  // Bluetooth classic channels (2402–2480 MHz) - NRF24L01 channels are the same plus 2 MHz
+  for (int i = 0; i < 79; i++) {  
+    bluetooth_channels[i] = i + 2; // 2402 MHz + (i MHz)
+  }
 
   // Start serial monitor for debugging
   Serial.begin(115200);
-  Serial.println();
-  Serial.println("BLE Jammer starting...");
+  Serial.println("\n--- FOR EDUCATIONAL PURPOSES ONLY! ---\nThis code repository is for educational purposes only!\nDO NOT use a signal jamming device as they can can interfere with public safety communications and emergency services.\nIn many countries the use of signal jammers is prohibited by law and intentional interference with wireless signals can result in severe penalties.\n");
   
-  // Give radios extra time to start up
-  delay(1000);
+  Serial.println("Bluetooth jammer is starting...");
+
+  // Setup Button & LED
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  // digitalWrite(LED_PIN, LOW);
 
   // Check radios have started ok
-  if (!radio1.begin()) {
+  while (!radio1.begin()) {
     Serial.println("Radio 1 init failed. Check wiring.");
-    while (1); // Stop until reboot
+
+    // Recheck radio every 5 seconds
+    delay(5000);
+    if(radio1.failureDetected) {
+      radio1.begin();                          // Attempt to re-configure the radio with defaults
+      radio1.failureDetected = 0;              // Reset the detection value
+    }
   }
-  if (!radio2.begin()) {
+  while (!radio2.begin()) {
     Serial.println("Radio 2 init failed. Check wiring.");
-    while (1); // Stop until reboot
+
+    // Recheck radio every 5 seconds
+    delay(5000);
+    if(radio2.failureDetected) {
+      radio2.begin();                          // Attempt to re-configure the radio with defaults
+      radio2.failureDetected = 0;              // Reset the detection value
+    }
   }
 
   // Configure radio modules settings
   setupRadio(radio1);
   setupRadio(radio2);
 
-  // Setup Button & LED
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+  // if(radio1.isPVariant()) { Serial.println("Radio 1 is PVariant"); }else{ Serial.println("Radio 1 is NOT PVariant"); }
+  // if(radio2.isPVariant()) { Serial.println("Radio 2 is PVariant"); }else{ Serial.println("Radio 2 is NOT PVariant"); }
 
-  Serial.println("Setup complete.");
+  delay(2000);
+  
+  Serial.println("Setup complete.\n\nPress BOOT to start jamming...\nPress it again to switch to BT classic mode...\n");
 }
 
 void setupRadio(RF24& radio) {
   // Radio settings
   radio.setAutoAck(false);                // Disable auto acknowledgement
   radio.setRetries(0, 0);                 // No retries
+  radio.disableCRC();                     // Disable CRC validation
   radio.setDataRate(RF24_2MBPS);          // High rate to increase traffic
-  radio.setPALevel(RF24_PA_MAX);          // Max power
-  radio.setCRCLength(RF24_CRC_DISABLED);  // Disable CRC validation
-  // radio.disableCRC();
+  radio.setPALevel(RF24_PA_MAX, 0);       // Max power
+  radio.setChannel(ble_channels[0]);      // Start on first channel
   radio.setPayloadSize(32);               // Standard BLE packet size
   radio.stopListening();                  // Transmit mode
-  // radio.setAddressWidth(5);
-  // radio.startConstCarrier(RF24_PA_MAX, 2);
+  radio.powerUp();
+
+  radio.setAddressWidth(3); // Shorter address means faster channel switching
+  radio.setAutoAck(0x00); // Fully disabled auto ack
+}
+
+void setClasicMode() {
+  // Switch from BLE only to Bluetooth classic mode
+  num_channels = sizeof(bluetooth_channels) / sizeof(bluetooth_channels[0]);
+  split_index = num_channels / 2;
+  endCh1 = split_index - 1;
+  startCh2 = split_index;
+  endCh2 = num_channels - 1;
+
+  // Classic mode flag
+  classicMode = true;
 }
 
 void loop() {
-  // Set starting channel of each antenna
-  static uint8_t ch1 = startCh1;
-  static uint8_t ch2 = startCh2;
+  static int hopCount = 0;
 
   // Get debugging info using serial monitor
   if (Serial.available() > 0) {
-    String serialInput = Serial.readString();
+    String serialInput = Serial.readStringUntil('\n');
     serialInput.trim();
 
     // If 'debug1' or 'debug2' is typed then show debugging info for that radio
@@ -106,53 +148,85 @@ void loop() {
   // If the button is pressed then start/stop jamming
   buttonState = digitalRead(BUTTON_PIN);
   if(buttonState == LOW && lastButtonState == HIGH ) {
-    if(jammingOn) {
+    if(jammingOn && classicMode) {
       // Stop jamming
       jammingOn = false;
 
       // Turn the LED off
       digitalWrite(LED_PIN, LOW);
+
+      Serial.println("\nTransmission stopped.\n");
+
+      // Reset device
+      ESP.restart();
+    }else if(jammingOn) {
+      // Switch modes and continue
+      setClasicMode();
+
+      Serial.println("\nSwitched to bluetooth classic channels...\n");
     }else{
       // Start jamming
       jammingOn = true;
 
       // Turn the LED on
       digitalWrite(LED_PIN, HIGH);
+
+      Serial.println("Start transmitting...");
     }
+    delay(200); // Button debounce
   }
   lastButtonState = buttonState;
 
-  // If the jammer has been started
-  if(jammingOn) {
-    // Payloads that will be transmitted
-    uint8_t payload1[32];
-    uint8_t payload2[32];
-
-    // Fill payloads with random data
-    for (int i = 0; i < 32; i++) {
-      payload1[i] = random(0, 256);
-      payload2[i] = random(0, 256);
-    }
-
-    // Radio 1 transmit the random data
-    radio1.setChannel(bluetooth_channels[ch1]);
-    radio1.write(&payload1, sizeof(payload1));
-    Serial.println("Radio 1 transmit on channel " + String(bluetooth_channels[ch1]));
+  // If the jammer has been started and its past the hop interval time
+  if (jammingOn && (micros() - lastHopTime >= hopInterval)) {
+    sendRandomPacket();
     
-    // Switch radio 1 to next channel
-    ch1++;
-    if (ch1 > endCh1) ch1 = startCh1;
+    hopCount++;
+    lastHopTime = micros();
 
-    // Radio 2 transmit the random data
-    radio2.setChannel(bluetooth_channels[ch2]);
-    radio2.write(&payload2, sizeof(payload2));
-    Serial.println("Radio 2 transmit on channel " + String(bluetooth_channels[ch2]));
-
-    // Switch radio 2 to next channel
-    ch2++;
-    if (ch2 > endCh2) ch2 = startCh2;
-
-    // Short delay to avoid locking up
-    delayMicroseconds(300);
+    // Debug output every 5 seconds
+    if (millis() - (micros() * 1000) >= 5000 && hopCount) {
+      Serial.print("Transmitted to");
+      if(classicMode) {
+        Serial.print(" classic");
+      }else{
+        Serial.print(" BLE");
+      }
+      Serial.print(" channels " + String(hopCount) + " times.");
+      Serial.println();
+    }
   }
+}
+
+void sendRandomPacket() {
+  static uint8_t ch1 = 0;
+  static uint8_t ch2 = startCh2;
+
+  // Generate random payload
+  uint8_t payload[32];
+  for (uint8_t i = 0; i < 32; i++) payload[i] = random(256);
+
+  if (classicMode) {
+    // In classic mode each radio takes care of one half of the channels
+    radio1.setChannel(bluetooth_channels[ch1]);
+    radio2.setChannel(bluetooth_channels[ch2]);
+    
+    ch1 = (ch1 + 1);
+    if(ch1 > endCh1) ch1 = 0;
+    ch2 = (ch2 + 1);
+    if(ch2 > endCh2) ch2 = startCh2;
+  } else {
+    // BLE advertising channels
+    radio1.setChannel(ble_channels[ch1]);
+    radio2.setChannel(ble_channels[ch2]);
+
+    ch1 = (ch1 + 1);
+    if(ch1 > endCh1) ch1 = 0;
+    ch2 = (ch2 + 1);
+    if(ch2 > endCh2) ch2 = startCh2;
+  }
+
+  // Transmit payload
+  radio1.startFastWrite(payload, sizeof(payload), true);
+  radio2.startFastWrite(payload, sizeof(payload), true);
 }
